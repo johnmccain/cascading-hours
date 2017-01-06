9d63c2d3e80c00b363fdb432dcd18d17396b8410
John McCain <johnm.freestate@gmail.com>
2017-01-06 13:12:06 -0600

    Add API and debug

--- api.php
+++ api.php
@@ -1,25 +1,25 @@
 <?
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: text/html; charset=ISO-8859-1');
-include 'cascading_hours.module';
+// drupal root and include
+define('DRUPAL_ROOT', '/var/www/public/drupal');
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 
-if($_GET['insert']) {
-	$location_id = ch_create_locatio('KJHK');
-	$rule_id = ch_create_rule($location_id, 0, strtotime('last Monday'), strtotime('next Thursday'));
-	ch_create_schedule($rule_id, 0, strtotime('12:00 am'), strtotime('5:00 pm'));
-	ch_create_schedule($rule_id, 1, strtotime('9:00 am'), strtotime('5:00 pm'));
-	ch_create_schedule($rule_id, 2, strtotime('9:00 am'), strtotime('5:00 pm'));
-	ch_create_schedule($rule_id, 3, strtotime('8:00 am'), strtotime('6:00 pm'));
-	ch_create_schedule($rule_id, 4, strtotime('12:00 am'), strtotime('5:00 pm'));
-	ch_create_schedule($rule_id, 5, strtotime('12:00 am'), strtotime('5:00 pm'));
-	ch_create_schedule($rule_id, 6, strtotime('12:00 am'), strtotime('5:00 pm'));
+
+// Load Drupal
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
+drupal_load('module', 'cascading_hours');
+
+
+$location = htmlspecialchars($_GET['location']);
+$start_date = $_GET['start_date'];
+$end_date = $_GET['end_date'];
+
+$schedule = ch_get_schedule_in_range_for_location_with_name($location, $start_date, $end_date);
+if($schedule['error']) {
+	echo json_encode($schedule);
 } else {
-	$location = $_GET['location'];
-	$start_date = $_GET['start_date'];
-	$end_date = $_GET['end_date'];
-
-	$schedule = ch_get_schedule_in_range_for_location_with_name($location, $start_date, $end_date);
 	$obj = ch_generate_schedule($schedule, $start_date);
 	echo json_encode($obj);
-
 }
