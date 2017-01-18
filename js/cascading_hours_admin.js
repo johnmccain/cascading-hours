1a0b346b3517d1d705334744bbeafe5be94dc482
John McCain <johnm.freestate@gmail.com>
2017-01-18 15:07:57 -0600

    Add admin UI for schedules

--- js/cascading_hours_admin.js
+++ js/cascading_hours_admin.js
@@ -1,3 +1,13 @@
 jQuery(document).ready( function() {
 	// jQuery(".datepicker").datepicker({dateFormat: 'mm/dd/yy'});
+	console.log('test');
+	jQuery('.timepicker').timepicker({
+	    timeFormat: 'h:mm p',
+	    interval: 15,
+	    defaultTime: '11',
+	    startTime: '00:00',
+	    dynamic: false,
+	    dropdown: true,
+	    scrollbar: true
+	});
 });
