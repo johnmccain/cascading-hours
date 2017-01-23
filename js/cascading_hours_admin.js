811ea761d1e105b6271d8c5dcacc6213caecbfb1
John McCain <johnm.freestate@gmail.com>
2017-01-23 14:06:20 -0600

    Setup CRON for deleting old rules

--- js/cascading_hours_admin.js
+++ js/cascading_hours_admin.js
@@ -1,13 +1,29 @@
 jQuery(document).ready( function() {
-	// jQuery(".datepicker").datepicker({dateFormat: 'mm/dd/yy'});
-	console.log('test');
-	jQuery('.timepicker').timepicker({
-	    timeFormat: 'h:mm p',
-	    interval: 15,
-	    defaultTime: '11',
-	    startTime: '06:00',
-	    dynamic: false,
-	    dropdown: true,
-	    scrollbar: true
+	try {
+		jQuery('.timepicker').timepicker({
+			timeFormat: 'h:mm p',
+			interval: 15,
+			defaultTime: '11',
+			startTime: '06:00',
+			dynamic: false,
+			dropdown: true,
+			scrollbar: true
+		});
+	} catch(e) {
+		//timepicker not loaded, do nothing
+	}
+
+	jQuery('.numeric-field').map(function() {
+		this.onkeydown = liveValidateNumericField;
+		this.onkeyup = liveValidateNumericField;
 	});
 });
+
+/**
+ * Removes any non-numeric characters from the value.
+ * For use on text fields that should have numeric values only. Should be used as an event handler
+ */
+function liveValidateNumericField() {
+	var val = jQuery(this).val();
+	jQuery(this).val(val.replace(/[^0-9]/g, ''));
+}
